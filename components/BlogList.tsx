afa93a4de3911cb649ef8979231bfd038debee3f
Andrés <andressantana99@hotmail.se>
2023-01-22 20:05:23 +0100

    Added style and route

---
+++ components/BlogList.tsx
@@ -0,0 +1,60 @@
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import urlFor from "../lib/urlFor";
+import ClientSideRoute from "./ClientSideRoute";
+
+type Props = {
+  posts: Post[];
+};
+
+function BlogList({ posts }: Props) {
+  return (
+    <div>
+      <hr className="border-[#6b0a7f] mb-10" />
+      <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
+        {posts.map((post) => (
+          <ClientSideRoute route={`/post/${post.slug.current}`} key={post._id}>
+            <div className="flex flex-col group cursor-pointer">
+              <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+                <Image
+                  className="object-cover object-left lg:object-center"
+                  src={urlFor(post.mainImage).url()}
+                  alt={post.author.name}
+                  fill
+                />
+                <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+                  <div>
+                    <p className="font-bold">{post.title}</p>
+                    <p>
+                      {new Date(post._createdAt).toLocaleDateString("en-SE", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}
+                    </p>
+                  </div>
+                  <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                    {post.categories.map((category) => (
+                      <div className="bg-[#6b0a7f] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                        <p>{category.title}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5 flex-1">
+                <p className="underline text-lg font-bold">{post.title}</p>
+                <p className="text-gray-500 line-clamp-2">{post.description}</p>
+              </div>
+              <p className="flex items-center mt-5 font-bold group-hover:underline">
+                Read Post <ArrowUpRightIcon className="h-4 w-4 ml-2" />
+              </p>
+            </div>
+          </ClientSideRoute>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BlogList;
