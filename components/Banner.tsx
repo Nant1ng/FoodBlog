576573c83284f5699a7dea44645421ea32110dd6
Andrés <andressantana99@hotmail.se>
2023-01-22 15:41:26 +0100

    Small design changes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,9 @@
 function Banner() {
   return (
-    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+    <div className="flex flex-col  font-bold px-10 py-5 mb-10">
       <div>
         <h1 className="text-7xl">Nantings Weekly Food Blog</h1>
-        <h2 className="mt-5 md:mt-0">
+        <h2 className="pt-5">
           Welcome to{" "}
           <span className="underline decoration-4 decoration-[#6b0a7f]">
             Every Foodies{" "}
